9b54ad564fca98f3dcf470e8440b9248efb928cf
Jan Pudewills <jan.pudewills@gmail.com>
2016-09-15 19:58:11 +1000

    v0.1.2

--- Readme.md
+++ Readme.md
@@ -1,7 +1,17 @@
-# features
-- imports and compiles images, fonts, svgs, react, sass
+# pack
+- simple build process cli for JS projects (node and browser)
+- babel (airbnb, react), webpack, sourcemaps and hot-reloading ready to go
+
+## web
+- imports and bundles scripts, stylesheets (sass), images, fonts, svgs and jsx
 - development server with hot reload and component hot reload
 - builds optimised production bundle with assets
+- proxy to sit on top of existing server
+
+## node
+- full es6 support
+- hot-reloading with source maps
+- builds bundle for node v4 and above
 
 # get started
 ## demo
@@ -17,16 +27,20 @@
     {
       "scripts": {
         "start": "pack -w",
-        "build": "pack"
+        "build": "pack",
+        "help": "pack -h"
       }
     }
 
 # folder structure
-## normal
     - src
       - main.js
 
-## react
+# react
+enable react mode to mount `export default Component` from `src/main` into `#render` or an empty `div`.
+enables component hot reload while preserving state. if a directory `src/components` exists, components can be imported as modules. (`import Component from 'Component';` resolves to `src/components/Component.js`).
+
+## folder structure
     - src
       - components
         - App
@@ -34,35 +48,18 @@
           - style.scss
       - main.js
 
-
 # options
-`-w`, `--watch` [false]
-enable watch mode and start development server with hot reload on localhost:port
-
-`-r`, `--react` [false]
-enable react mode
-expect entry file (src/main) to export component on default
-enables component hot reload
-mounts the component into #render (or empty div)
-
-`-p`, `--port` [8080]
-port number for dev server (ignored in non-watch mode)
-
-`-o`, `--output` ['dist']
-output directory relative to working directory for production bundles (ignored in watch mode)
-
-`-c`, `--components` ['components']
-react component directory relative to source
-a component Component.js in that directory can be imported from 'Component'
-
-`-s`, `--source` ['src']
-source directory relative to working directory
-
-`-a`, `--assets` ['/']
-assets url relative to hostname
-
-`-b`, `--bundle` ['bundle']
-bundle name (bundle.js and bundle.css)
-
-`-m`, `--main` ['main']
-entry point file name
+    .version('0.1.3')
+    .description('pack a bundle')
+    .option('-n, --node', 'enable node mode', false)
+    .option('-w, --watch', 'enable watch mode', false)
+    .option('-r, --react', 'enable react', false)
+    .option('-s --src [srcdir]', 'source directory [src]', 'src')
+    .option('-o, --output [directory]', 'output directory [dist]', 'dist')
+    .option('-a --assets [pubdir]', 'assets directory []', '')
+    .option('-p, --port [port]', 'port number [8080]', 8080)
+    .option('--bundle [bundle]', 'bundle name [bundle]', 'bundle')
+    .option('--main [main]', 'main filename [main]', 'main')
+    .option('--static [staticdir]', 'static folder for development server []', '')
+    .option('--proxy [proxy]', 'proxy port or server []', '')
+    .option('--components [compdir]', 'react component dir [components]', 'components')
--- util/config.js
+++ util/config.js
@@ -13,93 +13,128 @@ const ensureExists = fn => {
   }
 };
 
-module.exports = ({ root, output, react, source, watch, port, components, assets, bundle, main }) => ({
-  entry: {
-    app: [
-      (watch && react) && 'react-hot-loader/patch',
-      watch && `webpack-dev-server/client?http://localhost:${port}/`,
-      watch && 'webpack/hot/dev-server',
-      !react && path.join(root, source, main),
-      react && path.join(__dirname, 'react'),
+const nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter((x) => ['.bin'].indexOf(x) === -1)
+  .forEach((mod) => {
+    nodeModules[mod] = `commonjs ${mod}`;
+  });
+
+module.exports = (options) => {
+  const root = options.root;
+  const output = options.output;
+  const react = options.react;
+  const src = options.src;
+  const watch = options.watch;
+  const port = options.port;
+  const components = options.components;
+  const assets = options.assets;
+  const bundle = options.bundle;
+  const main = options.main;
+  const node = options.node;
+  const proxy = !!options.proxy;
+  return {
+    entry: [
+      (node && watch) && 'webpack/hot/poll?1000',
+      (!node && watch && react) && 'react-hot-loader/patch',
+      (!node && watch) && `webpack-dev-server/client?http://localhost:${port}/`,
+      (!node && watch) && 'webpack/hot/dev-server',
+      (node || !react) && path.join(root, src, main),
+      (!node && react) && path.join(__dirname, 'react'),
     ].filter(filter => !!filter),
-  },
-  output: {
-    path: path.join(root, output),
-    publicPath: assets,
-    filename: `${bundle}.js`,
-  },
-  context: root,
-  resolve: {
-    extensions: ['', '.js', '.json', '.scss', '.css'],
-    fallback: [
-      react && ensureExists(path.join(root, source, components)),
-    ].filter(filter => !!filter),
-    alias: {
-      main: path.join(root, source, main),
+    output: {
+      path: path.join(root, output),
+      publicPath: assets,
+      filename: `${bundle}.js`,
     },
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel',
-        exclude: /node_modules/,
-        query: {
-          presets: ['airbnb'],
-          plugins: [
-            (react && watch) && 'react-hot-loader/babel',
-            ['babel-root-slash-import', { rootPathSuffix: source }],
-          ].filter(filter => !!filter),
+    target: node ? 'node' : 'web',
+    externals: node ? nodeModules : {},
+    context: root,
+    resolve: {
+      extensions: [''].concat([
+        '.js',
+        '.json',
+        !node && '.scss',
+        !node && '.css',
+      ].filter(filter => !!filter)),
+      fallback: [
+        react && ensureExists(path.join(root, src, components)),
+      ].filter(filter => !!filter),
+      alias: {
+        main: path.join(root, src, main),
+      },
+    },
+    watch: !!watch,
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          loader: 'babel',
+          exclude: /node_modules/,
+          query: {
+            presets: ['airbnb'],
+            plugins: [
+              (react && watch && !node) && 'react-hot-loader/babel',
+              ['babel-root-slash-import', { rootPathSuffix: src }],
+            ].filter(filter => !!filter),
+          },
         },
-      },
-      {
-        test: /\.json$/,
-        loader: 'json',
-      },
-      {
-        test: /\.(woff|ttf|eot|woff2|svg)$/,
-        loaders: ['url?limit=25000'],
-      },
-      watch && {
-        test: /(\.scss|\.css)$/,
-        loaders: ['style', 'css', 'sass'],
-      },
-      watch && {
-        test: /\.(png|jpg|gif)$/,
-        loaders: [
-          'url?limit=25000',
-        ],
-      },
-      !watch && {
-        test: /(\.scss|\.css)$/,
-        loader: ExtractTextPlugin.extract('style', 'css!sass'),
-      },
-      !watch && {
-        test: /\.(png|jpg|gif)$/,
-        loaders: [
-          'url?limit=25000',
-          'image-webpack?optimizationLevel=7&interlaced=false',
-        ],
-      },
+        {
+          test: /\.json$/,
+          loader: 'json',
+        },
+        !node && {
+          test: /\.(woff|ttf|eot|woff2|svg)$/,
+          loaders: ['url?limit=25000'],
+        },
+        (!node && watch) && {
+          test: /(\.scss|\.css)$/,
+          loaders: ['style', 'css', 'sass'],
+        },
+        (!node && watch) && {
+          test: /\.(png|jpg|gif)$/,
+          loaders: [
+            'url?limit=25000',
+          ],
+        },
+        (!node && !watch) && {
+          test: /(\.scss|\.css)$/,
+          loader: ExtractTextPlugin.extract('style', 'css!sass'),
+        },
+        (!node && !watch) && {
+          test: /\.(png|jpg|gif)$/,
+          loaders: [
+            'url?limit=25000',
+            'image-webpack?optimizationLevel=7&interlaced=false',
+          ],
+        },
+      ].filter(filter => !!filter),
+    },
+    devtool: watch ? 'eval-source-map' : 'source-map',
+    plugins: [
+      new webpack.NoErrorsPlugin(),
+      watch && new webpack.HotModuleReplacementPlugin(),
+      watch && new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('development'),
+      }),
+      !watch && new webpack.optimize.OccurenceOrderPlugin(),
+      !watch && new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false,
+        },
+      }),
+      (!node && !watch) && new ExtractTextPlugin(`${bundle}.css`),
+      !watch && new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('production'),
+      }),
+      (!node && watch && !proxy) && new HtmlWebpackPlugin({}),
+      node && new webpack.IgnorePlugin(/\.(css|scss)$/),
+      node && new webpack.BannerPlugin('require("source-map-support").install();', { raw: true, entryOnly: false }),
+
     ].filter(filter => !!filter),
-  },
-  devtool: watch ? 'eval-source-map' : 'source-map',
-  plugins: [
-    new webpack.NoErrorsPlugin(),
-    watch && new webpack.HotModuleReplacementPlugin(),
-    watch && new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development'),
-    }),
-    !watch && new webpack.optimize.OccurenceOrderPlugin(),
-    !watch && new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-      },
-    }),
-    !watch && new ExtractTextPlugin(`${bundle}.css`),
-    !watch && new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-    }),
-    watch && new HtmlWebpackPlugin({}),
-  ].filter(filter => !!filter),
-});
+    node: {
+      __dirname: false,
+      __filename: false,
+    },
+  };
+};
